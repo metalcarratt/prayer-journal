147b61e80224ecf125a47fc6ed8b5f1b56265a63
Caleb Garratt <metal.carratt@gmail.com>
2022-05-16 08:30:36 +1200

    Add google login button

--- src/contacts/List.js
+++ src/contacts/List.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import NewContact from './NewContact.js';
+import GoogleLogin from 'react-google-login';
 import './list.css';
 
 function ListContacts() {
@@ -17,6 +18,10 @@ function ListContacts() {
         window.location.reload();
     }
 
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <div className="page">
             <h1>Progressive Prayer Journal</h1>
@@ -25,6 +30,13 @@ function ListContacts() {
             )}
             <NewContact />
             <a href="#" onClick={clickReset}>Reset</a>
+            <GoogleLogin
+                clientId="846763549713-kdc8d3umettaee43303tgpnbggqhvf0t.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     )
 }
